28f74351a2dcd97826d028ae469dbe95a2b05746
Maksymilian Bilski <biger9@o2.pl>
2023-05-08 22:09:57 +0200

    Update app.js

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
