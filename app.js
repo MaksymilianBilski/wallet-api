fca5f40b576352db04a8c9186a5cc1c71baf6196
Maksymilian Bilski <biger9@o2.pl>
2023-05-08 13:41:53 +0200

    add authorization header to CORS

--- app.js
+++ app.js
@@ -21,8 +21,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept",
-    "authorization"
+    "Origin, X-Requested-With, Content-Type, Accept, authorization"
   );
   next();
 });
